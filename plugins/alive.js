88328d85b3861a3315a2bc66ee95ea0b9714febd
Sahas-Tech75 <sahasnethsara2010@gmail.com>
2024-09-06 17:21:42 +0530

    Create alive.js

---
+++ plugins/alive.js
@@ -0,0 +1,34 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "alive",
+    desc: "Check bot online or no.",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let des = `*👋 Hello ${pushname}*
+
+*╔╭────────────╮╕*
+*╭│I'm Alive Now👾  │─◎◎▷*
+*╘╰────────────╯╜*
+*│A QUEEN-KYLIE-MD │Whatsapp Bot Based │Many │Services With A │RealTime* *Automated │Consversational* *││Experience, Enjoy💫.*
+*| So,I Think This Bots Are Useful To You.📍*
+*│*
+*│Type .menu To Get Bot User*
+*│Menu💫*
+*╰───────────────◎◎▷*
+ *➣ ʏᴏᴜᴛᴜʙᴇ⚡*= https://www.youtube.com/@Sahas_Tech
+
+*➣ ᴡʜᴀᴛꜱᴀᴘᴘ ᴄʜᴀɴɴᴇʟ⚡* = https://whatsapp.com/channel/0029VaiTjMlK5cDLek3bB533
+
+*©Qᴜᴇᴇɴ ᴋʏʟɪᴇ-ᴍᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜッ*`
+return await conn.sendMessage(from,{image: {url: `https://telegra.ph/file/9b44e3266c065df601430.jpg`},caption: des},{quoted: mek})
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
