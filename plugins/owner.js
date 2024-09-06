5e458cc12c7df19ae6ff2d5013fa2571fbe91530
Sahas-Tech75 <sahasnethsara2010@gmail.com>
2024-09-06 17:03:35 +0530

    Create owner.js

---
+++ plugins/owner.js
@@ -0,0 +1,31 @@
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "owner",
+    desc: "owner the bot",
+    category: "main",
+    react: "👨‍💻",
+    filename: __filename
+},
+
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let dec = `*👋 Hello ${pushname}*
+
+> *MY OWNER INFO* 
+
+*⚡ᴏᴡɴᴇʀ ɴᴀᴍᴇ -: ꜱᴀʜᴀꜱ ɴᴇᴛʜꜱᴀʀᴀ (ꜱᴀʜᴀꜱ ᴛᴇᴄʜ)*
+*⚡ɴᴜᴍʙᴇʀ* -: 94718913389
+*⚡ʏᴏᴜᴛᴜʙᴇ -:* https://www.youtube.com/@Sahas_Tech
+*⚡ᴡʜᴀᴛꜱᴀᴘᴘ ᴄʜᴀɴɴᴇʟ-:* https://whatsapp.com/channel/0029VaiTjMlK5cDLek3bB533
+
+*©Qᴜᴇᴇɴ ᴋʏʟɪᴇ-ᴍᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜッ*
+`
+await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/8e7b91d1b98bf020d15a2.jpg`},caption:dec},{quoted:mek});
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
