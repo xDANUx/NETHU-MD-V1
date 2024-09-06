5ab9827e451d5a4b99596d5e9383d2da2bc171c3
Sahas-Tech75 <sahasnethsara2010@gmail.com>
2024-09-06 16:57:57 +0530

    Create system.js

---
+++ plugins/system.js
@@ -0,0 +1,28 @@
+const {cmd , commands} = require('../command')
+const os = require("os")
+const {runtime} = require('../lib/functions')
+
+cmd({
+    pattern: "system",
+    alias: ["status","botinfo"],
+    desc: "check up time",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{ 
+
+let status =`┌────────────────────
+    *├ ⏳Uptime:-  ${runtime(process.uptime())}*    
+    *├ 📟Ram usage:- ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB*
+    *├ ⚙️HostName:- ${os.hostname()}*
+    *├ 👨‍💻Owner:- Sahas Nethsara*   
+    *├ 🤖versions:- 1.0.0*
+    └───────────────────────
+    *©Qᴜᴇᴇɴ ᴋʏʟɪᴇ-ᴍᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜッ*`
+return reply(`${status}`)
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
