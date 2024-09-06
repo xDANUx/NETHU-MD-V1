ced172bd3413ae1882025dd3b962f0f54d7419fb
Sahas-Tech75 <sahasnethsara2010@gmail.com>
2024-09-06 16:48:43 +0530

    Create song+video.js

---
+++ song+video.js
@@ -0,0 +1,90 @@
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yt-search')
+
+cmd({
+    pattern: "song",
+    desc: "download songs",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("*Please enter a query or a url!*")
+const search = await yts(q)
+const data = search.videos[0]
+const url = data.url
+
+let desc = `*🎼 QUEEN-KYLIE-MD SONG DOWNLOADER . .⚙️*
+
+🎼⚙️ TITLE - ${data.title}
+
+🎼⚙️ VIEWS - ${data.views}
+
+🎼⚙️ DESCRIPTION - ${data.description}
+
+🎼⚙️ TIME - ${data.timestamp}
+
+🎼⚙️ AGO - ${data.ago}
+
+*©Qᴜᴇᴇɴ ᴋʏʟɪᴇ-ᴍᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜッ*
+`
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+//download audio
+
+let down = await fg.yta(url)  
+let downloadUrl = down.dl_url
+
+//send audio
+await conn.sendMessage(from,{audio:{url: downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+await conn.sendMessage(from,{document:{url: downloadUrl},mimetype:"audio/mpeg",fileName:data.title + "mp3",caption:"*©Qᴜᴇᴇɴ ᴋʏʟɪᴇ-ᴍᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜッ*"},{quoted:mek})
+}catch(e){
+reply(`${e}`)
+}
+})
+
+//===========video-dl===========
+
+cmd({
+    pattern: "video",
+    desc: "download video",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("*Please enter a query or a url!*")
+const search = await yts(q)
+const data = search.videos[0]
+const url = data.url
+
+let des = `*📽️ QUEEN-KYLIE-MD VIDEO DOWNLOADER . .⚙️*
+
+📽️⚙️ TITLE - ${data.title}
+
+📽️⚙️ VIEWS - ${data.views}
+
+📽️⚙️ DESCRIPTION - ${data.description}
+
+📽️⚙️ TIME - ${data.timestamp}
+
+📽️⚙️ AGO - ${data.ago}
+
+*©Qᴜᴇᴇɴ ᴋʏʟɪᴇ-ᴍᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜッ*
+`
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:des},{quoted:mek});
+
+//download video
+
+let down = await fg.ytv(url)  
+let downloadUrl = down.dl_url
+
+//send video
+await conn.sendMessage(from,{video:{url: downloadUrl},mimetype:"video/mp4"},{quoted:mek})
+await conn.sendMessage(from,{document:{url: downloadUrl},mimetype:"video/mp4",fileName:data.title + "mp4",caption:"*©Qᴜᴇᴇɴ ᴋʏʟɪᴇ-ᴍᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜッ*"},{quoted:mek})
+    
+}catch(a){
+reply(`${a}`)
+}
+})
