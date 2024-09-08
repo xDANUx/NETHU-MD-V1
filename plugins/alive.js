06d15a72c4a384f1b3dc69f7869eff363cb6119e
Sahas-Tech75 <sahasnethsara2010@gmail.com>
2024-09-08 10:53:37 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -11,7 +11,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 const voice = {
-    alive: 'media/AUD-20240904-WA0426.mp3'
+    alive: 'media/AUD-20240908-WA0480'
 }
 
 let des = `*👋 Hello ${pushname}*
