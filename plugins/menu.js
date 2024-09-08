fbda9eb8553a92682fcc7146b8ef230768225730
Sahas-Tech75 <sahasnethsara2010@gmail.com>
2024-09-08 11:06:19 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -11,7 +11,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 const voice = {
-    alive: 'AUD-20240904-WA0432.mp3'
+    alive: 'AUD-20240904-WA0426.mp3'
 }
 
 let dec = `*👋 Hello ${pushname}*
