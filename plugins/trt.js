90e98cb023a3f7c06b1fc817200bd804b7c064e1
nethu-cloud <nnethmika100@gmail.com>
2024-09-17 20:01:33 +0530

    Update trt.js

--- plugins/trt.js
+++ plugins/trt.js
@@ -28,7 +28,7 @@ async (conn, mek, m, { from, q, reply }) => {
 🔠 *Translated*: ${translation}
 🌐 *Language*: ${targetLang.toUpperCase()}
 
-*©Qᴜᴇᴇɴ ᴋʏʟɪᴇ-ᴍᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜッ*`;
+> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴇᴛʜᴍɪᴋᴀ ᴍᴀɪɴ*`;
 
         return reply(translationMessage);
     } catch (e) {
