ed0b8f3f6f2d18cece302c0345a15b4364a4a257
xDANUx <162179100+xDANUx@users.noreply.github.com>
2024-10-06 23:27:47 -0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ SESSION_ID: process.env.SESSION_ID || "UyVlmKDa#2akcd7rPAfthLrRMIh8Ux0at3a1VCkEr
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/Rj6sSqR/d42734f1-a858-4ea1-8ae5-7b004302ad40.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello I'm alive now",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
 SUDO_NB: process.env.SUDO_NB || "94704227534",
 OWNER_NAME: process.env.OWNER_NAME || "Nethmika MAIN",
 BOT_NAME: process.env.BOT_NAME || "DANU-BOT",
