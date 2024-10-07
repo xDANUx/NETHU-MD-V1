f033eb0ee3df21bad4426888705c86daf9ddbc15
xDANUx <162179100+xDANUx@users.noreply.github.com>
2024-10-06 23:26:50 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id ",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://imgtr.ee/images/2024/09/17/37f9be869d1404ef1a5d188178450537.jpeg",
+SESSION_ID: process.env.SESSION_ID || "UyVlmKDa#2akcd7rPAfthLrRMIh8Ux0at3a1VCkErUJnivVmJdUI",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/Rj6sSqR/d42734f1-a858-4ea1-8ae5-7b004302ad40.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello I'm alive now",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 SUDO_NB: process.env.SUDO_NB || "94704227534",
 OWNER_NAME: process.env.OWNER_NAME || "Nethmika MAIN",
-BOT_NAME: process.env.BOT_NAME || "NETHU-MD",
+BOT_NAME: process.env.BOT_NAME || "DANU-BOT",
 AUTO_VOICE: process.env.AUTO_VOICE || "true"
 
 };
